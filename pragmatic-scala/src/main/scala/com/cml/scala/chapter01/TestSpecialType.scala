2533577bebb2156b99bdd77a05adc728b9220fbc
wuque <swchenminglei@163.com>
2022-11-24 11:50:00 +0800

    chapter01

---
+++ pragmatic-scala/src/main/scala/com/cml/scala/chapter01/TestSpecialType.scala
@@ -0,0 +1,19 @@
+package com.cml.scala.chapter01
+
+/**
+ * @title: TestSpecialType
+ * @projectName pragmatic-scala
+ * @description: Unit类型、Null类型和Nothing类型
+ * @author wuque
+ * @date 2022/11/2317:14
+ */
+object TestSpecialType {
+
+  def main(args: Array[String]): Unit = {
+    def sayOk: Unit = { // unit 表示没有返回值，即void
+      }
+
+    println(sayOk)
+  }
+
+}
